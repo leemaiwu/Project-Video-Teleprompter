aa50b590e4bf9267215756cc59f25971bce2ee5a
Laura Wu <leemaiwu@gmail.com>
2023-07-14 13:02:39 -0600

    Pushing commits before implementing modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Video from './components/Video'
 import Script from './components/Script'
 
 function App() {
+
+  const [scriptText, setScriptText] = useState('')
+
   return (
     <div className='App'>
       <Routes>
-        <Route index element={<Script />} />
-        <Route path='/video' element={<Video />}/>
+        <Route index element={<Script handleScriptText={setScriptText} />} />
+        <Route path='/video' element={<Video scriptText={scriptText} />}/>
       </Routes>
     </div>
   )
--- src/components/Script.jsx
+++ src/components/Script.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Script.css'
 
-const Script = () => {
+const Script = ({handleScriptText}) => {
+
+    const defaultScriptText =
+        "Bonjour from France! I'm exploring the stunning landscapes and vibrant culture of this beautiful country. There's a cafe just around the corner. I'm just walking around the Tuileries Garden Park now. I will give you all an update at my next stop. Merci! Bye for now."
+    
+    const [scriptText, setScriptText] = useState(defaultScriptText)
+
+    const handleScriptChange = (event) => {
+        const updatedScriptText = event.target.value
+        setScriptText(updatedScriptText || defaultScriptText)
+        handleScriptText(updatedScriptText || defaultScriptText)
+    }
+
   return (
     <div className='script-container'>
         <div className='script-title'>  
             <h1>Your Teleprompter Script</h1>
         </div>
         <p className='script-edit'>Edit as needed:</p>
-        <textarea className='teleprompter-text'>
-            Bonjour from France! I'm exploring the stunning landscapes and vibrant culture of this beautiful country. There's a cafe just around the corner. I'm just walking around the Tuileries Garden Park now. I will give you all an update at my next stop. Merci! Bye for now.
-        </textarea>
+        <textarea
+            className="teleprompter-text"
+            value={scriptText}
+            onChange={handleScriptChange}
+        />
         <nav>
-            <Link to='/video'>
+        <Link to={{ pathname: '/video', state: {scriptText: scriptText || defaultScriptText} }}>
                 <button className='record-button'>Record</button>
-            </Link>
+        </Link>
         </nav>
     </div>
   )
