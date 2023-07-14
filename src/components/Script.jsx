a1908c01821cd3cc431eda198ba852ab35b1482c
Laura Wu <leemaiwu@gmail.com>
2023-07-14 01:36:57 -0600

    Basic functionality for script area and video controls

--- src/components/Script.jsx
+++ src/components/Script.jsx
@@ -8,12 +8,13 @@ const Script = () => {
         <div className='script-title'>  
             <h1>Your Teleprompter Script</h1>
         </div>
+        <p className='script-edit'>Edit as needed:</p>
         <textarea className='teleprompter-text'>
-            Bonjour from France! I'm exploring the stunning landscapes and vibrant culture of this beautiful country. There's a cafe just around the corner. I'm just walking around the Tuileries Garden park now. I will give you all an update at my next stop. Merci! Bye for now.
+            Bonjour from France! I'm exploring the stunning landscapes and vibrant culture of this beautiful country. There's a cafe just around the corner. I'm just walking around the Tuileries Garden Park now. I will give you all an update at my next stop. Merci! Bye for now.
         </textarea>
         <nav>
             <Link to='/video'>
-                <button className='record-button'>Record Video</button>
+                <button className='record-button'>Record</button>
             </Link>
         </nav>
     </div>
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,23 +1,65 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { HiOutlineChevronLeft } from "react-icons/hi";
+import { HiOutlineChevronLeft } from 'react-icons/hi'
+import { FaPlay } from 'react-icons/fa'
 import '../styles/Video.css'
 import video from '../video/mobile_travel_vlog.mp4'
 
 const Video = () => {
 
-  return (
+    const videoRef = useRef(null)
+    const [ isPlaying, setIsPlaying ] = useState(false)
+    const [showPlayButton, setShowPlayButton] = useState(true)
+
+    const handleVideoClick = () => {
+        let video = videoRef.current
+        if (isPlaying) {
+            video.pause()
+        } else {
+            video.play()
+        }
+        setIsPlaying(!isPlaying)
+    }
+
+    const handleVideoPlay = () => {
+        setIsPlaying(true)
+        setShowPlayButton(false)
+    }
+
+    const handleVideoPause = () => {
+        setIsPlaying(false)
+        setShowPlayButton(true)
+    }
+
+    return (
     <div className='video-wrapper'>
-    <nav>
-        <Link to='..'>
-            <HiOutlineChevronLeft className="back-icon" />
-        </Link>
-    </nav>
-        <video className="video-self" controls>
-            <source src={video} type="video/mp4" />
+        <nav>
+            <Link to='..'>
+                <HiOutlineChevronLeft className='back-icon-light' />
+            </Link>
+        </nav>
+        <nav>
+            <Link to='..'>
+                <HiOutlineChevronLeft className='back-icon-dark' />
+            </Link>
+        </nav>
+        <video 
+            className='video-self'
+            ref={videoRef}
+            onClick={handleVideoClick}
+            onPlay={handleVideoPlay}
+            onPause={handleVideoPause}
+            playsInline
+        >
+            <source src={video} type='video/mp4' />
         </video>
+        {!isPlaying && showPlayButton && (
+            <div className={`play-button ${showPlayButton ? 'fade-in' : ''}`} onClick={handleVideoClick}>
+                <FaPlay className='play-icon' />
+            </div>
+        )}
     </div>
-  )
+    )
 }
 
 export default Video
